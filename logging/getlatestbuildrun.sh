a63e61e7815a5aad4c6ad17a93f6649fb34ca836
rickroty <github.ok@roty.com>
2017-10-13 14:50:49 -0700

    Update getlatestbuildrun.sh

--- logging/getlatestbuildrun.sh
+++ logging/getlatestbuildrun.sh
@@ -3,4 +3,4 @@ git reset --hard origin/master
 
 docker build -t igatemonitor:latest /home/pi/projects/igatemonitor/logging/
 
-docker run -it -p"24224:24224" -v "/home/pi/projects/igatemonitor/logging/:/etc/" igatemonitor:latest
+docker run -it -p"24224:24224" -v "/home/pi/projects/igatemonitor/logging/:/etc/" logging:latest
