bb4bb403e1acfa9ab60c4ea2f6972f6e0262c790
rickroty <github.ok@roty.com>
2017-10-13 12:17:00 -0700

    Update getlatestbuildrun.sh

--- getlatestbuildrun.sh
+++ getlatestbuildrun.sh
@@ -1,6 +1,6 @@
 git fetch origin
 git reset --hard origin/master
 
-docker build -t igatemonitor:v1 .
+docker build -t igatemonitor:v1 /home/pi/projects/igatemonitor/v1/
 
 docker run -it -p"24224:24224" -v "/home/pi/:/etc/" igatemonitor:v1
