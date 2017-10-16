4f321b4b23200b4ddb3270716d65d70c058238e8
rickroty <github.ok@roty.com>
2017-10-15 21:58:50 -0700

    Update getmaster.sh

--- getmaster.sh
+++ getmaster.sh
@@ -1,3 +1,5 @@
 sudo git fetch origin
 sudo git reset --hard origin/master
+
+mkdir /home/pi/projects/igatemonitor/logging/bigquery
 cp /home/pi/projects/KG7TMT-10-7b0b688525e7.json /home/pi/projects/igatemonitor/logging/bigquery/
