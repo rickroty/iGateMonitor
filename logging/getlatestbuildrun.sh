9d1ffb0a6f77c41c13947799fc96452bd3475c03
rickroty <github.ok@roty.com>
2017-10-15 21:36:31 -0700

    Update getlatestbuildrun.sh

--- logging/getlatestbuildrun.sh
+++ logging/getlatestbuildrun.sh
@@ -1,5 +1,6 @@
 git fetch origin
 git reset --hard origin/master
+cp /home/pi/projects/KG7TMT-10-7b0b688525e7.json /home/pi/projects/igatemonitor/logging/bigquery/
 
 docker build -t logging:latest /home/pi/projects/igatemonitor/logging/
 
