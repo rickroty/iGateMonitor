e18bc9318074f8161cc771ca0e1b8989397a7415
rickroty <github.ok@roty.com>
2017-10-15 22:17:14 -0700

    Update getlatestbuildrun.sh

--- monitoring/getlatestbuildrun.sh
+++ monitoring/getlatestbuildrun.sh
@@ -7,4 +7,4 @@ cp /home/pi/projects/KG7TMT-10-7b0b688525e7.json /home/pi/projects/igatemonitor/
 
 docker build -t monitoring:latest /home/pi/projects/igatemonitor/monitoring/
 
-docker run -p 24224:24224 -v /home/pi/projects/igatemonitor:/host -it monitoring:latest
+docker run -v /home/pi/projects/igatemonitor:/host -it monitoring:latest
