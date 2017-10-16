e7ff9c31f3491c9d7ea06ef3f54a09a295c2adfe
rickroty <github.ok@roty.com>
2017-10-15 20:15:14 -0700

    Update iwconfigparser.py

--- monitoring/iwconfigparser.py
+++ monitoring/iwconfigparser.py
@@ -19,9 +19,15 @@ for a in adapters:
     if a[:5] == "wlan0":
         wlan0=a.split("  ")
 
-for reading in wlan0:
-    if len(reading.translate(None, ' \n\t\r'))>0:
-        print reading.strip()    
+if wlano.count > 0:
+    for reading in wlan0:
+        if len(reading.translate(None, ' \n\t\r'))>0:
+            if reading.strip()[:6]=="Signal":
+                #signal_strength = reading.split("=").split(" ")[0]
+                print "signal_strength:" + reading.split("=").split(" ")[0] 
+            #if reading.strip()[:4]=="Link":
+            #    link_quality = int(reading.split("=").split("/")[0]) / int(reading.split("=").split("/")[1]) 
+            print reading.strip()    
 
         
         
