9d6beb4e19f646b0d23a503cee0e98be66112d4d
rickroty <github.ok@roty.com>
2017-10-15 19:09:17 -0700

    Update iwconfigparser.py

--- monitoring/iwconfigparser.py
+++ monitoring/iwconfigparser.py
@@ -2,15 +2,16 @@ import sys
 import string
 
 fname=''
+content=''
+
 if len(sys.argv) > 1:
     fname = sys.argv[1]
 else:
     print(" No filename specified. ")
     quit
 
-F = open(fname,”r”)
-
-input = ""
+with open(fname) as f:
+    content = f.read()
 
 try:
     c = F.read()
