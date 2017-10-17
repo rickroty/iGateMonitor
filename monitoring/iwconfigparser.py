2745c4a80eb31c7fd6cb868eb1810d815fe88ef9
rickroty <github.ok@roty.com>
2017-10-16 17:44:02 -0700

    Update iwconfigparser.py

--- monitoring/iwconfigparser.py
+++ monitoring/iwconfigparser.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 import string
@@ -34,10 +35,10 @@ if wlan0.count > 0:
             if reading.strip()[:4]=="Link":
                 try:  link_quality = int(reading.split("=")[1].split("/")[0])*100 / int(reading.split("=")[1].split("/")[1]) 
                 except ZeroDivisionError: link_quality=0
-                    
+			
             dt = datetime.datetime.now()
-                    
-            requests.post('http://GK7TMT-10.local:24224', json={ \
+	    hostname=os.environ['HOSTIP']
+		requests.post('http://' + hostname + ':24224', json={ \
 	            "APRS_station": "KG7TMT-10", \
 	            "SSID": "ICU2", \
 	            "signal_strength": "+ signal_strength +", \
