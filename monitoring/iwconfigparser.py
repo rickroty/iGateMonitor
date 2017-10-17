6aad6df8da05289cf5bb6ed9a8d69670a11a7092
rickroty <github.ok@roty.com>
2017-10-16 18:14:59 -0700

    Update iwconfigparser.py

--- monitoring/iwconfigparser.py
+++ monitoring/iwconfigparser.py
@@ -25,27 +25,31 @@ for a in adapters:
         wlan0=a.split("  ")
 
 if wlan0.count > 0:
-    for reading in wlan0:
-        if len(reading.translate(None, ' \n\t\r'))>0:
-            signal_strength=0
-            link_quality=0
-            if reading.strip()[:6]=="Signal":
-                signal_strength = reading.split("=")[1].split(" ")[0]
-                #print "signal_strength:" + reading.split("=")[1].split(" ")[0]
-            if reading.strip()[:4]=="Link":
-                try:  link_quality = int(reading.split("=")[1].split("/")[0])*100 / int(reading.split("=")[1].split("/")[1]) 
-                except ZeroDivisionError: link_quality=0
-			
-            dt = datetime.datetime.now()
-	    hostname=os.environ['HOSTIP']
-	requests.post('http://' + hostname + ':24224', json={ \
-	            "APRS_station": "KG7TMT-10", \
-	            "SSID": "ICU2", \
-	            "signal_strength": "+ signal_strength +", \
-                "link_quality": "+ str(link_quality) +", \
-	            "date": "+ dt.strftime('%s') +" \
-                }) 
-
+	for reading in wlan0:
+		if len(reading.translate(None, ' \n\t\r'))>0:
+            		signal_strength=0
+			link_quality=0
+            		if reading.strip()[:6]=="Signal":
+				signal_strength = reading.split("=")[1].split(" ")[0]
+				#print "signal_strength:" + reading.split("=")[1].split(" ")[0]
+			if reading.strip()[:4]=="Link":
+				try:  link_quality = int(reading.split("=")[1].split("/")[0])*100 / int(reading.split("=")[1].split("/")[1]) 
+ 				except ZeroDivisionError: link_quality=0
+			dt = datetime.datetime.now()
+			hostname=os.environ['HOSTIP']
+			uri='http://' + hostname + ':24224'
+			data=
+			print uri
+			print data='{ \
+	                            "APRS_station": "KG7TMT-10", \
+	                            "SSID": "ICU2", \
+	                            "signal_strength": '+ signal_strength +', \
+                                    "link_quality": '+ str(link_quality) +', \
+	                            "date": '+ dt.strftime("%s") +" \
+                                    }'
+			try:
+				r = requests.post(uri, json=data)
+				print "response status=" + r.status_code
 
 print "done!"
 quit
