50d1c3431f01da655993723067cd94becb214b0f
rickroty <github.ok@roty.com>
2017-10-16 19:25:49 -0700

    Update iwconfigparser.py

--- monitoring/iwconfigparser.py
+++ monitoring/iwconfigparser.py
@@ -22,36 +22,43 @@ adapters = filecontent.split("\n\n")
 
 for a in adapters:
     if a[:5] == "wlan0":
+	print "wlan0"
         wlan0=a.split("  ")
 
 	if wlan0.count > 0:
+		logit=False
 		for reading in wlan0:
+			print ">" + reading
 			if len(reading.translate(None, ' \n\t\r'))>0:
             			signal_strength=0
 				link_quality=0
             			if reading.strip()[:6]=="Signal":
 					signal_strength = reading.split("=")[1].split(" ")[0]
+					logit=True
 					#print "signal_strength:" + reading.split("=")[1].split(" ")[0]
 				if reading.strip()[:4]=="Link":
 					try:  link_quality = int(reading.split("=")[1].split("/")[0])*100 / int(reading.split("=")[1].split("/")[1]) 
  					except ZeroDivisionError: link_quality=0
-				dt = datetime.datetime.now()
-				timestamp = dt.strftime('%Y-%m-%d %H:%M:%S')
-				print 'time:' + timestamp
-				hostname = os.environ['HOSTIP']
-				uri='http://' + hostname + ':24224'
-				data='{'
-				data+='\"APRS_station\": \"KG7TMT-10\",' 
-				data+='"SSID": "ICU2",'
-		                data+='\"signal_strength\": \"'+ str(signal_strength) +'\",'
-				data+='\"link_quality\": \"'+ str(link_quality) +'\",'
-		                data+='\"date\": \"'+ timestamp + '\"}'
-				print "Writing stats to: " + uri
-				print data
-				r = requests.post(uri, json=data)
-				print "response status=" + str(r.status_code)
+					logit=True
+		
+		if logit:		
+			dt = datetime.datetime.now()
+			timestamp = dt.strftime('%Y-%m-%d %H:%M:%S')
+			print 'time:' + timestamp
+			hostname = os.environ['HOSTIP']
+			uri='http://' + hostname + ':24224'
+			data='{'
+			data+='\"APRS_station\": \"KG7TMT-10\",' 
+			data+='"SSID": "ICU2",'
+			data+='\"signal_strength\": \"'+ str(signal_strength) +'\",'
+			data+='\"link_quality\": \"'+ str(link_quality) +'\",'
+			data+='\"date\": \"'+ timestamp + '\"}'
+			print "Writing stats to: " + uri
+			print data
+			r = requests.post(uri, json=data)
+			print "response status=" + str(r.status_code)
 	else:
 		"wlan0 not found."
 
-	print "done!"
-	quit
+print "done!"
+quit
