cb96a27f8bb9601678fc4cf3ec8aa70de4972ba2
rickroty <github.ok@roty.com>
2017-10-29 17:33:24 -0700

    Update run.sh

--- logging/run.sh
+++ logging/run.sh
@@ -1,1 +1,9 @@
-docker run -p 24224:24224 -v /home/pi/projects/igatemonitor:/host -it logging:latest
+if [ ! "$(docker ps -q -f name=logging:latest)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=logging:latest)" ]; then
+        # cleanup
+        docker rm logging:latest
+    fi
+    docker run -d --name logging:latest -p 24224:24224 -v /home/pi/projects/igatemonitor:/host -it logging:latest
+fi
+
+
