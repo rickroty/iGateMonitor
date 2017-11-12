dd9781d0d278b3d912a41d68df89808c8d2dcc9a
rickroty <github.ok@roty.com>
2017-11-11 22:58:58 -0700

    Update runcontainer.sh

--- monitoring/runcontainer.sh
+++ monitoring/runcontainer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #
-docker run -v /home/pi/projects/igatemonitor:/host -e HOSTIP=$(python /home/pi/projects/igatemonitor/checkip.py) --network=host  -i monitoring:latest 
+docker run -v /home/pi/projects/igatemonitor:/host -e HOSTIP=$(python /home/pi/ projects/igatemonitor/checkip.py) --network=host  --device /dev/ttyAMA0:/dev/ttyAMA0 --device /dev/mem:/dev/mem --privileged -it
+monitoring:latest
 
