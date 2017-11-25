4b03f45e95e31d2b13db1b42cb82e0cabe479563
rickroty <github.ok@roty.com>
2017-11-25 12:54:11 -0700

    Update runcontainer.sh

--- monitoring/runcontainer.sh
+++ monitoring/runcontainer.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 #
-docker run -v /home/pi/projects/igatemonitor:/host -e HOSTIP=$(python /home/pi/projects/igatemonitor/checkip.py) --network=host  --device /dev/ttyAMA0:/dev/ttyAMA0 --device /dev/mem:/dev/mem --privileged  monitoring:latest
+if [ ! "$(docker ps -q -f name=monitoring)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=monitoring)" ]; then
+        # cleanup
+        docker rm monitoring
+    fi
+    docker run --name monitoring -v /home/pi/projects/igatemonitor:/host -e HOSTIP=$(python /home/pi/projects/igatemonitor/checkip.py) --network=host  --device /dev/ttyAMA0:/dev/ttyAMA0 --device /dev/mem:/dev/mem --privileged  monitoring:latest
+fi
+
+
+
 
